be379a961cffe9e373de88412066159ed8ad9894
Nicholas Lim <nicknitewolf@gmail.com>
2025-01-17 09:24:33 +0800

    Project: Use esbuild to compile tools and reorganise tools

--- eslint.config.js
+++ eslint.config.js
@@ -30,6 +30,6 @@ export default tseslint.config(
   },
   {
     // Temporarily ignoring the MangaDex extension until it gets rewritten
-    ignores: ["bundles", "src/MangaDex"],
+    ignores: ["bundles", "dist", "src/MangaDex"],
   },
 );
