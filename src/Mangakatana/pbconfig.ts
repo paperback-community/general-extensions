dc8df0a251ab929d31645ab6e5e12dc16412118b
Egwau Godfrey <egwaujunior@gmail.com>
2025-03-05 00:54:36 +0300

    Update src/Mangakatana/pbconfig.ts
    Co-authored-by: Eduard Smet <contact@celarye.dev>

--- src/Mangakatana/pbconfig.ts
+++ src/Mangakatana/pbconfig.ts
@@ -2,7 +2,7 @@ import { ContentRating, SourceInfo, SourceIntents } from "@paperback/types";
 
 export default {
   name: "MangaKatana",
-  description: "Mangakatana extension",
+  description: "Extension that pulls content from mangakatana.com.",
   version: "1.0.0-alpha.1",
   icon: "icon.png",
   language: "en",
