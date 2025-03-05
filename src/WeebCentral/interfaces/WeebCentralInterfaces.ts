5dc3cd7ab9c575e6559cdf230c414a5523547aba
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2025-03-05 08:22:40 +0800

    Remove unused interfaces

--- src/WeebCentral/interfaces/WeebCentralInterfaces.ts
+++ src/WeebCentral/interfaces/WeebCentralInterfaces.ts
@@ -1,19 +1,3 @@
-import { Tag } from "@paperback/types";
-
-export interface StatusTypes {
-  ONGOING: string;
-  HIATUS: string;
-  COMPLETED: string;
-  DROPPED: string;
-}
-
-export interface Filters {
-  seriesTypes: Array<Tag>;
-  genres: Array<Tag>;
-  seriesStatuses: Array<Tag>;
-  order: Array<Tag>;
-}
-
 export interface WeebCentralMetadata {
   page?: number;
   offset?: number;
